a2566db05eb20ead4e25b52db06757cc1392a087
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-11-07 08:43:04 +0100

    feat: add server crate with CLI args and workspace

---
+++ crates/server/src/args.rs
@@ -0,0 +1,30 @@
+#[derive(clap::Args, Debug, Clone)]
+pub struct ServerArgs {
+    #[arg(
+        short,
+        long,
+        env,
+        num_args = 0..,
+        value_delimiter = ',',
+        long_help = "The port to run the application on",
+    )]
+    pub allowed_origins: Vec<String>,
+    #[arg(
+        short = 'H',
+        long = "server-host",
+        env = "SERVER_HOST",
+        name = "SERVER_HOST",
+        default_value = "0.0.0.0",
+        long_help = "The host to run the application on"
+    )]
+    pub host: String,
+    #[arg(
+        short = 'P',
+        long = "server-port",
+        env = "SERVER_PORT",
+        name = "SERVER_PORT",
+        default_value_t = 3333,
+        long_help = "The port to run the application on"
+    )]
+    pub port: u16,
+}
